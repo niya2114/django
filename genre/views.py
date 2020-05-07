b0cb59d07651d45b09321feab024cb70aa5504ea
SoumyaRanjanNaik <srnaik2209@gmail.com>
2020-05-07 21:11:47 +0530

    made corrections to forms.py and edited the templates for registration and upgraded bootstrap from 3.0 to 4.1

---
+++ genre/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Collection, Piece
+from django.views import generic
+from .forms import UserForm
+from django.contrib.auth import authenticate, login
+from django.views.generic import View
+
+
+class index(generic.ListView):
+    template_name = "genre/genretemplate.html"
+
+    def get_queryset(self):
+        return Collection.objects.all()
+
+
+class details(generic.DetailView):
+    model = Collection
+    template_name = "genre/detailstemplate.html"
+
+
+class UserFormView(View):
+    form_class = UserForm
+    template_name = "genre/formtemplate.html"
+
+    def get(self, request):
+        form = self.form_class(None)
+        return render(request, self.template_name, {"form": form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            user.set_password(password)
+            user.save()
+            newuser = authenticate(username=username, password=password)
+
+            if newuser is not None:
+                if newuser.is_active:
+                    login(request, newuser)
+                    return redirect("/genre")
+
+        return render(request, self.template_name, {"form": form})
